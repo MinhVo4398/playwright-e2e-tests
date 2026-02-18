4ac81f96e174282777faca82142989da722c99a8
minhvo <minhvo4398@gmail.com>
2026-02-18 11:31:59 +0700

    feat: Add Allure Playwright reporter support and update README with an image.

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from "@playwright/test";
 
 /**
  * Read environment variables from file.
@@ -12,7 +12,7 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './tests',
+  testDir: "./tests",
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -22,20 +22,40 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    [
+      "html",
+      {
+        open: "never", // Don't auto-open HTML report
+      },
+    ],
+    [
+      "allure-playwright",
+      {
+        detail: true,
+        suiteTitle: true,
+        environmentInfo: {
+          name: "TEST",
+          appName: "CURA",
+          Release: "Release 1.1",
+          node_version: process.version,
+        },
+      },
+    ],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    trace: 'on-first-retry',
+    trace: "on-first-retry",
     ignoreHTTPSErrors: true,
     navigationTimeout: 30_000,
-    screenshot: 'on'
+    screenshot: "on",
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
     },
 
     // {
